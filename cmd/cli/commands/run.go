ebe90e55f3b0671ce2eff2f647f92aa67f5b19fd
Mishal-Alajmi <mishalajmi.dev@gmail.com>
2026-09-12 01:16:06 +0300

    feat: added project loading in run command

--- cmd/cli/commands/run.go
+++ cmd/cli/commands/run.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"fmt"
 
+	"github.com/mishalalajmi/mimic/internal/project"
+	"github.com/mishalalajmi/mimic/internal/server"
 	"github.com/spf13/cobra"
 )
 
@@ -10,8 +12,14 @@ var runCommand = &cobra.Command{
 	Use:   "run",
 	Short: "Start the mimic mock server",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Starting mimic...")
-		return nil
+		p, err := project.Load("examples/example-project/")
+		if err != nil {
+			return fmt.Errorf("loading project: %w", err)
+		}
+
+		srv := server.New(":4000", p)
+		fmt.Printf("Starting Mimic project %q...\n", p.Name)
+		return srv.Start()
 	},
 }
 
